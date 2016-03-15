b767a21a03a3940f0f7c1fc4a400d89e7caf93af
matthewsotoudeh <matthewsot@outlook.com>
2016-03-14 21:49:50 -0700

    carousel made

--- content/scripts/carousel.js
+++ content/scripts/carousel.js
@@ -1,30 +1,47 @@
 var animationDuration = 0.5;
-$(".carousel .forward").click(function() {
-    var carousel = $(this).closest(".carousel");
+function updateSelector() {
+    var selector = $(".carousel-selector");
+    selector.css("transition", "background-color " + animationDuration + "s linear");
+    selector.css("background-color", $(".carousel .active").css("background-color"));
+}
+
+function moveCarouselTo(next, reverse) {
+    var carousel = $(".carousel");
     var current = carousel.find(".active");
-    var next = current.next("article");
-    if (next.length <= 0 || next === current) {
-        next = carousel.find("article").first();
-    }
 
     current.removeClass("active");
     next.addClass("active");
 
-    current.css("animation", "carousel-out-forward " + animationDuration + "s forwards linear 1");
-    next.css("animation", "carousel-in-forward " + animationDuration + "s forwards linear 1");
+    var forwardBackward = reverse ? "backward" : "forward";
+    current.css("animation", "carousel-out-" + forwardBackward + " " + animationDuration + "s forwards linear 1");
+    next.css("animation", "carousel-in-" + forwardBackward + " " + animationDuration + "s forwards linear 1");
+    updateSelector();
+}
+
+$(".carousel .forward").click(function() {
+    var current = $(this).closest(".carousel").find(".active");
+    var next = current.next("article");
+
+    if (next.length <= 0 || next === current) {
+        next = $(".carousel").find("article").first();
+    }
+
+    moveCarouselTo(next, false);
 });
 
 $(".carousel .back").click(function() {
-    var carousel = $(this).closest(".carousel");
-    var current = carousel.find(".active");
+    var current = $(this).closest(".carousel").find(".active");
     var next = current.prev("article");
+
     if (next.length <= 0 || next === current) {
-        next = carousel.find("article").last();
+        next = $(".carousel").find("article").last();
     }
 
-    current.removeClass("active");
-    next.addClass("active");
+    moveCarouselTo(next, true);
+});
 
-    current.css("animation", "carousel-out-backward " + animationDuration + "s forwards linear 1");
-    next.css("animation", "carousel-in-backward " + animationDuration + "s forwards linear 1");
+$(".carousel-selector li").click(function() {
+    var next = $(".carousel").find("." + $(this).attr("class"));
+    var reverse = next.index() - $(".carousel .active").index() < 0;
+    moveCarouselTo(next, reverse);
 });
